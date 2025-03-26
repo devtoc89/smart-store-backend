5a723f389d655b719130f85328506fe424d3e883
devtoc89 <devtoc89@gmail.com>
2025-03-27 01:22:52 +0900

    chore: deploy script

--- smartstore-base/src/main/java/com/smartstore/api/v1/common/infrastructure/S3Config.java
+++ smartstore-base/src/main/java/com/smartstore/api/v1/common/infrastructure/S3Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -13,32 +14,53 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
-  @Value("${cloud.aws.s3.credentials.access-key}")
+  @Value("${cloud.aws.s3.credentials.access-key:}")
   private String accessKey;
 
-  @Value("${cloud.aws.s3.credentials.secret-key}")
+  @Value("${cloud.aws.s3.credentials.secret-key:}")
   private String secretKey;
 
   @Value("${cloud.aws.s3.credentials.region}")
   private String region;
 
+  private boolean hasStaticKey() {
+    return accessKey != null && !accessKey.isBlank() &&
+        secretKey != null && !secretKey.isBlank();
+  }
+
   @Bean
   public S3Client s3Client() {
     Region awsRegion = Region.of(region);
-    AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+    if (hasStaticKey()) {
+      return S3Client.builder()
+          .region(awsRegion)
+          .credentialsProvider(StaticCredentialsProvider.create(
+              AwsBasicCredentials.create(accessKey, secretKey)))
+          .build();
+    }
+
     return S3Client.builder()
         .region(awsRegion)
-        .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+        .credentialsProvider(DefaultCredentialsProvider.create())
         .build();
   }
 
   @Bean
   public S3Presigner s3Presigner() {
     Region awsRegion = Region.of(region);
+
+    if (hasStaticKey()) {
+      return S3Presigner.builder()
+          .region(awsRegion)
+          .credentialsProvider(StaticCredentialsProvider.create(
+              AwsBasicCredentials.create(accessKey, secretKey)))
+          .build();
+    }
+
     return S3Presigner.builder()
         .region(awsRegion)
-        .credentialsProvider(StaticCredentialsProvider.create(
-            AwsBasicCredentials.create(accessKey, secretKey)))
+        .credentialsProvider(DefaultCredentialsProvider.create())
         .build();
   }
 }
